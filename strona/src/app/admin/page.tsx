8060249b794c471692c01ad390aa12f90f519850
Karol Niemczyk <krl.niemczyk@gmail.com>
2024-11-22 20:14:56 +0100

    added option to delete all at once or one fishing rod

--- strona/src/app/admin/page.tsx
+++ strona/src/app/admin/page.tsx
@@ -10,6 +10,7 @@ export default function AdminPage() {
     price: "",
     image: "",
   });
+  const [toDelId, setToDelId] = useState("");
   const product = {
     name: newProduct.name,
     price: parseFloat(newProduct.price),
@@ -34,53 +35,85 @@ export default function AdminPage() {
   // Obsługa dodawania nowego produktu
   const handleAddProduct = () => {
     axios.post("http://127.0.0.1:5000/wedki", product);
+    window.location.reload();
   };
-
+  const handleDelAllProducts = () => {
+    axios.delete("http://127.0.0.1:5000/wedki");
+    window.location.reload();
+  };
   // Obsługa zmian formularza dodawania
   const handleNewProductChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewProduct({ ...newProduct, [e.target.name]: e.target.value });
   };
-
+  const handleIdSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setToDelId(event.target.value);
+  };
+  const handleDelProduct = (toDelId: string) => {
+    axios.delete(`http://127.0.0.1:5000/wedki/${toDelId}`);
+    window.location.reload();
+  };
   return (
     <div className="admin-page">
-      <h1>Panel Administratora - Dodaj Produkt</h1>
+      <section className="flex flex-col items-center gap-5 py-10 px-16 bg-woda w-full min-h-screen bg-cover bg-center bg-fixed">
+        {/* Formularz dodawania produktu */}
+        <div className="flex flex-row gap-4 mt-8">
+          <input
+            type="text"
+            name="name"
+            placeholder="Nazwa wędki"
+            value={newProduct.name}
+            onChange={handleNewProductChange}
+            className="border-4 border-gray-600 p-4 rounded-xl"
+          />
+          <input
+            type="text"
+            name="price"
+            placeholder="Cena"
+            value={newProduct.price}
+            onChange={handleNewProductChange}
+            className="border-4 border-gray-600 p-4 rounded-xl"
+          />
+          <input
+            type="text"
+            name="image"
+            placeholder="URL do zdjęcia"
+            value={newProduct.image}
+            onChange={handleNewProductChange}
+            className="border-4 border-gray-600 p-4 rounded-xl"
+          />
+          <button
+            onClick={handleAddProduct}
+            className="bg-gray-400 border-4 border-gray-600 text-white py-2 px-4 rounded-xl active:scale-70"
+          >
+            Dodaj Wędkę
+          </button>
+        </div>
+        <div className="flex flex-row gap-4 mt-8">
+          <input
+            type="text"
+            name="id"
+            placeholder="Id wedki do usuniecia"
+            value={toDelId}
+            onChange={handleIdSearch}
+            className="border-4 border-gray-600 p-4 rounded-xl"
+          ></input>
+          <button
+            onClick={() => handleDelProduct(toDelId)}
+            className="bg-red-600 border-4 border-gray-600 text-white py-2 px-4 rounded-xl active:scale-70"
+          >
+            Usuń Wedke
+          </button>
+          <button
+            onClick={handleDelAllProducts}
+            className="bg-red-600 border-4 border-gray-600 text-white py-2 px-4 rounded-xl active:scale-70"
+          >
+            Usuń wszystkie
+          </button>
+        </div>
 
-      {/* Formularz dodawania produktu */}
-      <div className="flex flex-col gap-4 mt-8">
-        <input
-          type="text"
-          name="name"
-          placeholder="Nazwa wędki"
-          value={newProduct.name}
-          onChange={handleNewProductChange}
-          className="border-4 border-gray-600 p-4 rounded-xl"
-        />
-        <input
-          type="text"
-          name="price"
-          placeholder="Cena"
-          value={newProduct.price}
-          onChange={handleNewProductChange}
-          className="border-4 border-gray-600 p-4 rounded-xl"
-        />
-        <input
-          type="text"
-          name="image"
-          placeholder="URL do zdjęcia"
-          value={newProduct.image}
-          onChange={handleNewProductChange}
-          className="border-4 border-gray-600 p-4 rounded-xl"
-        />
-        <button
-          onClick={handleAddProduct}
-          className="bg-blue-500 text-white py-2 px-4 rounded"
-        >
-          Dodaj Wędkę
-        </button>
-      </div>
-
-      {/* Podgląd produktów za pomocą komponentu Products */}
-      <Products products={products} searchQuery="" sortOrder="default" />
+        {/* Podgląd produktów za pomocą komponentu Products */}
+        <Products products={products} searchQuery="" sortOrder="default" />
+      </section>
     </div>
   );
 }
